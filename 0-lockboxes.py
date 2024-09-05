30498607e58a2e62b46aee7ce8883b15de595ba9
ojvl1 <carlosantompietri175@gmail.com>
2024-09-05 18:31:48 -0400

    the the code -  not finish

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -3,11 +3,16 @@
 def canUnlockAll(boxes):
 
     open_box = 0
+    keys = {}
 
-    for box in boxes:
-        if box == boxes:
-            open_box =+ 1
-            return True
-        else:
-            return False
-            
+    while boxes:
+        boxes[0].append(keys)
+        for i in boxes:
+            for y in boxes:
+                if keys == boxes[y]:
+                    boxes[y] = open_box
+                    open_box += 1
+                    open_box.append(keys)
+                    return True
+                else:
+                    return False
