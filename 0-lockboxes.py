7529a45914ac8ba55be7e45793c7823a3588a8a6
ojvl1 <antompietriosvaldo@gmail.com>
2024-09-05 11:40:34 -0400

    lockbox not finish

---
+++ 0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+
+    open_box = 0
+
+    for box in boxes:
+        if box == boxes:
+            open_box =+ 1
+            return True
+        else:
+            return False
+            
