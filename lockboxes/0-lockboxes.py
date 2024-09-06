695d8015f7d6dc79c9112097da072cacec18d35d
ojvl1 <antompietriosvaldo@gmail.com>
2024-09-05 22:03:34 -0400

    correct pycodestyle

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """Lockboxes Algorithm"""
 
+
 def canUnlockAll(boxes):
-    """Method that determines if all the boxes can be opened"""
+    """
+        Method that determines if
+        all the boxes can be opened
+    """
     unlocked = [False] * len(boxes)
     unlocked[0] = True
-    
     keys = [0]
-    
     while keys:
         current_key = keys.pop(0)
-        
         for key in boxes[current_key]:
             if key < len(boxes) and not unlocked[key]:
                 unlocked[key] = True
                 keys.append(key)
-    
     return all(unlocked)
