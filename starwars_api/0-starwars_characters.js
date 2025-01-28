e37003624679eed95c2948380932bddf235f43b9
ojvl1 <antompietriosvaldo@gmail.com>
2025-01-28 16:58:18 -0400

    Star Wars Characters

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error("Error:", error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error("Failed to fetch data:", response.statusCode);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error("Error:", error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.error("Failed to fetch character:", response.statusCode);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
