6cdc19d1ee4f367e3f53b5af0572f62f76807125
ojvl1 <antompietriosvaldo@gmail.com>
2025-02-25 16:12:15 -0400

    Change comes from within

---
+++ making_change/0-making_change.py
@@ -0,0 +1,10 @@
+def makeChange(coins, total):
+  if total <= 0:
+    return 0
+  dp = [float('inf')] * (total + 1)
+  dp[0] = 0
+  for amount in range(1, total + 1):
+    for coin in coins:
+      if coin <= amount:
+        dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+  return dp[total] if dp[total] != float('inf') else -1
