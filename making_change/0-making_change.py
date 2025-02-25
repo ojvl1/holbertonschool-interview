66640fcd90de631de00476d4ee51af6d71b83919
ojvl1 <antompietriosvaldo@gmail.com>
2025-02-25 16:13:57 -0400

    fix

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+
+"""
+Function that determine the fewest number 
+of coins needed to meet a given amount total
+"""
+
 def makeChange(coins, total):
   if total <= 0:
     return 0
