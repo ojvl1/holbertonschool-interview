31ae206a33619122f933865d7f6f1a7d134249d4
ojvl1 <antompietriosvaldo@gmail.com>
2025-02-25 19:34:40 -0400

    fix betty style

--- double_circular_linked_list/list.h
+++ double_circular_linked_list/list.h
@@ -16,9 +16,9 @@
  */
 typedef struct List
 {
-    char *str;
-    struct List *prev;
-    struct List *next;
+char *str;
+struct List *prev;
+struct List *next;
 } List;
 
 /* Function prototypes */
