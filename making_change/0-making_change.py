528ccb62e6901e2b2397b1cceebb09a69068bb35
ojvl1 <antompietriosvaldo@gmail.com>
2025-02-25 16:33:26 -0400

    fix pycodestyle

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
 """
-Function that determine the fewest number 
+Function that determine the fewest number
 of coins needed to meet a given amount total
 """
 
+
 def makeChange(coins, total):
-  if total <= 0:
-    return 0
-  dp = [float('inf')] * (total + 1)
-  dp[0] = 0
-  for amount in range(1, total + 1):
-    for coin in coins:
-      if coin <= amount:
-        dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-  return dp[total] if dp[total] != float('inf') else -1
+    """Return the fewest number ofd coins """
+    if total <= 0:
+        return 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if coin <= amount:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    return dp[total] if dp[total] != float('inf') else -1
