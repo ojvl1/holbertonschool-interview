55c91737d414ece82279c5a8d6b2104f0a1b9f11
ojvl1 <antompietriosvaldo@gmail.com>
2025-03-09 22:43:03 -0400

    pycodestyle

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -27,16 +27,16 @@ def island_perimeter(grid):
         for col in range(cols):
             if grid[row][col] == 1:
                 perimeter += 4
-                
+
                 if row > 0 and grid[row-1][col] == 1:
                     perimeter -= 1
-                
+
                 if row < rows-1 and grid[row+1][col] == 1:
                     perimeter -= 1
-                
+
                 if col > 0 and grid[row][col-1] == 1:
                     perimeter -= 1
-                
+
                 if col < cols-1 and grid[row][col+1] == 1:
                     perimeter -= 1
 
