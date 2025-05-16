8d957b5488113c8b3e824ec9a9659a3fdc8c7f75
ojvl1 <antompietriosvaldo@gmail.com>
2025-05-16 14:16:16 -0400

    pycodestyle

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 
 """
-  is_prime_sieve: 
-          Uses the Sieve of Eratosthenes 
+  is_prime_sieve:
+          Uses the Sieve of Eratosthenes
           algorithm to identify prime numbers and counts
           how many primes exist up to each index.
 
   isWinner:
           In each round, a set of consecutive integers
           from 1 to n is used. Players take
-          turns choosing a prime number and removing 
+          turns choosing a prime number and removing
           it and its multiples from the set.
-          The player who cannot make a move loses the round. 
+          The player who cannot make a move loses the round.
           Maria always goes first.
 """
 
+
 def is_prime_sieve(n):
     """
-    Generates a list of the number of prime numbers up to each number from 0 to n.
+    Generates a list of the number of prime
+    numbers up to each number from 0 to n.
     Args:
-        n (int): The maximum number up to which to generate prime counts.
+        n (int): The maximum number up to
+                which to generate prime counts.
 
     Returns:
-        List[int]: A list where the value at each index i is the count of prime
+        List[int]: A list where the value at
+                  each index i is the count of prime
                   numbers less than or equal to i.
     """
     sieve = [True] * (n + 1)
@@ -42,16 +46,19 @@ def is_prime_sieve(n):
 
 def isWinner(x, nums):
     """
-    Determines the winner of a series of prime number games.
+    Determines the winner of a
+    series of prime number games.
 
     Both players play optimally.
 
     Args:
         x (int): The number of rounds.
-        nums (List[int]): An array of integers where each value represents n for a round.
+        nums (List[int]): An array of integers
+        where each value represents n for a round.
 
     Returns:
-        str or None: The name of the player who won the most rounds ("Maria" or "Ben").
+        str or None: The name of the player who won the
+                    most rounds ("Maria" or "Ben").
                     If the number of wins is the same, returns None.
     """
     if x < 1 or not nums:
